e23ab1295532b239e6c839b016cbf838b401e4f4
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-05-07 10:21:12 -0700

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,14 +13,13 @@ def number_of_subscribers(subreddit):
         Returns:
            int: The number of subscribers, or 0 if the subreddit is invalid.
     """
-    url = f"https://www.reddit.com/r/subreddit/{subreddit}"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': "My Reddit API Client"}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             subscribers = data['data']['subscribers']
-            print("success")
             return subscribers
         else:
             return 0
