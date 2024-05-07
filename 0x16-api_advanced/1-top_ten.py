716bb445e97de4d8189a25303cc65f934594f99e
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-05-07 10:38:44 -0700

    API advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+This function queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'My Reddit API Client'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            for post in data[data][children]:
+                print(post[data][title])
+        else:
+            print("None")
+    except Exception as e:
+        print(f"Error: {e}")
+        print("None")
