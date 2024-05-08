f33c38036d47ef1a4765b1213afaa055ae958826
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-05-08 09:37:17 -0700

    Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,27 +1,26 @@
 # This puppet file automates to reset apache to return a webpage
 
 # Define an exec resource to run strace on the Apache process
-exec { strace_apache:
-  command     => strace -o /tmp/apache_strace.txt -f -s 1000 -p ,
-  path        => /bin:/usr/bin,
+exec { 'strace_apache':
+  command     => 'strace -o /tmp/apache_strace.txt -f -s 1000 -p $(pgrep apache2)',
+  path        => '/bin:/usr/bin',
   refreshonly => true,
 }
 
 # Define an exec resource to fix the issue based on the strace output
-exec { fix_apache_issue:
-  command     => sed -i s/display_errors = Off/display_errors = On/ /etc/php/5.5/apache2/php.ini,
-  path        => /bin:/usr/bin,
+exec { 'fix_apache_issue':
+  command     => 'sed -i "s/display_errors = Off/display_errors = On/" /etc/php/5.5/apache2/php.ini',
+  path        => '/bin:/usr/bin',
   refreshonly => true,
-  notify      => Exec[restart_apache],
+  notify      => Exec['restart_apache'],
 }
 
 # Define an exec resource to restart Apache after fixing the issue
-exec { restart_apache:
-  command     => service apache2 restart,
-  path        => /bin:/usr/bin,
+exec { 'restart_apache':
+  command     => 'service apache2 restart',
+  path        => '/bin:/usr/bin',
   refreshonly => true,
 }
 
 # Notify the restart_apache exec resource when the fix_apache_issue exec resource is executed
-Exec[fix_apache_issue] -> Exec[restart_apache]
-
+Exec['fix_apache_issue'] -> Exec['restart_apache']
